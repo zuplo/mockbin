dd2d9670de6583bd11976f8a4708bcc3768eab97
joshtwist <josh@zuplo.com>
2023-11-04 19:13:11 +0000

    updated operation names

--- modules/create-bin.ts
+++ modules/create-bin.ts
@@ -1,10 +1,10 @@
 import { ZuploContext, ZuploRequest } from "@zuplo/runtime";
-import { uploadFile } from "./file-utils";
+import { uploadResponse } from "./file-utils";
 
 export default async function (request: ZuploRequest, context: ZuploContext) {
   const binId = crypto.randomUUID();
   const content = await request.text();
-  await uploadFile(`${binId}.json`, content);
+  await uploadResponse(binId, content);
   return {
     id: binId
   }
