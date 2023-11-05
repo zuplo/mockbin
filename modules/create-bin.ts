57c339fbc36b9e73160a3dc43b71258f0fa88619
joshtwist <josh@zuplo.com>
2023-11-05 19:57:03 +0000

    changed to 201 response

--- modules/create-bin.ts
+++ modules/create-bin.ts
@@ -5,8 +5,12 @@ export default async function (request: ZuploRequest, context: ZuploContext) {
   const binId = crypto.randomUUID();
   const content = await request.json();
   await uploadResponse(binId, content);
-  return {
+  const response = {
     id: binId,
     url: `${binId}.api.mockbin.io`
   }
+  return new Response(JSON.stringify(response, null, 2), {
+    status: 201,
+    statusText: "Created"
+  });
 }
