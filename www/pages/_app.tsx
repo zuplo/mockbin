c455ee265db7492273dba185cae10bb734f41413
aabedraba <aabedraba@gmail.com>
2023-11-06 12:26:54 +0100

    add posthog

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -1,6 +1,37 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug()
+    },
+    capture_pageview: false // Disable automatic pageview capture, as we capture manually
+  })
+}
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = () => posthog?.capture('$pageview')
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
+  return <PostHogProvider client={posthog}>
+    <Component {...pageProps} />
+  </PostHogProvider>
 }
