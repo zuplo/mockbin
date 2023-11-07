6b2b0a1de4757c6330b113d0458c50819efc372e
aabedraba <aabedraba@gmail.com>
2023-11-07 16:46:00 +0100

    add og image support

---
+++ www/pages/_document.tsx
@@ -0,0 +1,44 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>Mockbin by Zuplo</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        {/* twitter og image */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@zuplo" />
+        <meta name="twitter:creator" content="@zuplo" />
+        <meta name="twitter:title" content="Mockbin by Zuplo" />
+        <meta
+          name="twitter:description"
+          content="Mockbin is a free tool that helps you mock an API endpoint and track requests to it."
+        />
+        <meta
+          name="twitter:image"
+          content="https://cdn.zuplo.com/assets/8e93df64-1a75-4cfe-afb7-10a99def9e0c.png"
+        />
+
+        {/* open graph */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://mockbin.io" />
+        <meta property="og:title" content="Mockbin by Zuplo" />
+        <meta
+          property="og:description"
+          content="Mockbin is a free tool that helps you mock an API endpoint and track requests to it."
+        />
+        <meta
+          property="og:image"
+          content="https://cdn.zuplo.com/assets/8e93df64-1a75-4cfe-afb7-10a99def9e0c.png"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
