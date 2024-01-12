27ac9ef5926eec51d8d29862b6c59f34e36ea305
Daniel Lehr <daniellehr@gmx.de>
2024-01-12 14:38:33 +0100

    Add "Create Bin" button for better visibility

--- www/components/BinHeader.tsx
+++ www/components/BinHeader.tsx
@@ -32,9 +32,25 @@ const BinHeader = ({
       </code>
       <CopyButton textToCopy={binUrl} />
     </div>
-    <button className="px-4" onClick={onRefresh}>
-      <RefreshIcon />
-    </button>
+    <div className="flex gap-4 px-4 items-center">
+      <Link
+        tabIndex={0}
+        href="/"
+        className={cn(
+          "relative rounded-md px-4 py-1 text-sm font-medium bg-gradient-to-b from-[#FF00BD] to-[#C0008F] opacity-85 transition-transform",
+          "hover:opacity-100 hover:scale-105",
+          "focus:ring-4 focus:outline-none focus:ring-[#C0008F]/50",
+        )}
+      >
+        Create Bin
+      </Link>
+      <button
+        className="px-2 py-2 hover:bg-slate-800 rounded"
+        onClick={onRefresh}
+      >
+        <RefreshIcon />
+      </button>
+    </div>
   </header>
 );
 export default BinHeader;
