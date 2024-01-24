45a5b3c6c284c6a26e0fc53e3c31266e471e3679
Daniel Lehr <daniellehr@gmx.de>
2024-01-24 10:31:43 +0100

    Use combobox dropdown for header name suggestions

--- www/components/HeaderRow.tsx
+++ www/components/HeaderRow.tsx
@@ -1,7 +1,13 @@
-import { useState } from "react";
+import { useMemo, useRef, useState } from "react";
+import { useCombobox } from "downshift";
+import { matchSorter } from "match-sorter";
+import cn from "classnames";
+
 import { Header } from "./HeadersList";
 import Input from "@/components/Input";
 import TrashIcon from "@/components/TrashIcon";
+import { CommonHeaders } from "@/utils/commonHeaders";
+import ArrowIcon from "@/components/ArrowIcon";
 
 interface HeaderRowProps {
   header: Header;
@@ -20,6 +26,7 @@ const HeaderRow = ({
 }: HeaderRowProps) => {
   const [keyError, setKeyError] = useState<string | undefined>();
   const [valueError, setValueError] = useState<string | undefined>();
+  const valueRef = useRef<HTMLInputElement | null>(null);
 
   const validateKey = (newKey: string) => {
     // Empty key validation
@@ -61,11 +68,6 @@ const HeaderRow = ({
     return true;
   };
 
-  const onChangeKey = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const isValid = validateKey(e.target.value);
-    onChange({ ...header, key: e.target.value, hasError: !isValid });
-  };
-
   const onChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     const isValid = validateValue(e.target.value);
     onChange({ ...header, value: e.target.value, hasError: !isValid });
@@ -74,16 +76,69 @@ const HeaderRow = ({
   const onClickDelete = () => {
     onDelete?.(header.id);
   };
+  const [inputValue, setInputValue] = useState(header.key);
+
+  const filteredHeaders = useMemo(
+    () => matchSorter(CommonHeaders, inputValue, {}),
+    [inputValue],
+  );
+
+  const {
+    isOpen,
+    getMenuProps,
+    getInputProps,
+    getItemProps,
+    getToggleButtonProps,
+    highlightedIndex,
+  } = useCombobox({
+    items: filteredHeaders,
+    initialInputValue: inputValue,
+    onSelectedItemChange: ({ selectedItem }) => {
+      if (!selectedItem) return;
+      onChange({ ...header, key: selectedItem });
+      setInputValue(selectedItem);
+      valueRef.current?.focus();
+      valueRef.current?.select();
+    },
+    onInputValueChange: ({ inputValue }) => {
+      if (!inputValue) return;
+      onChange({ ...header, key: inputValue });
+      setInputValue(inputValue);
+    },
+  });
 
   return (
     <div className="flex align-top items-start gap-2 flex-wrap sm:flex-nowrap">
-      <div className="w-full flex flex-col">
-        <Input
-          value={header.key}
-          placeholder="Key"
-          title={"Header key"}
-          onChange={onChangeKey}
-        />
+      <div className="w-full flex flex-col relative">
+        <Input placeholder="Key" title="Header key" {...getInputProps()} />
+        <button
+          type="button"
+          className="absolute right-1 text-slate-600 top-0 bottom-0 flex items-center"
+          {...getToggleButtonProps()}
+        >
+          <ArrowIcon className="rotate-90" />
+        </button>
+        <ul
+          className={cn(
+            !isOpen && "hidden",
+            "absolute z-10 top-full max-h-[200px] overflow-y-auto translate-y-1 w-full bg-slate-800 rounded-md",
+          )}
+          {...getMenuProps()}
+        >
+          {isOpen &&
+            filteredHeaders.map((item, index) => (
+              <li
+                className={cn(
+                  "p-2 cursor-pointer font-mono text-xs",
+                  highlightedIndex === index && "bg-slate-700",
+                )}
+                key={`${item}-${index}`}
+                {...getItemProps({ item, index })}
+              >
+                {item}
+              </li>
+            ))}
+        </ul>
         {keyError ? (
           <p className="mt-2 text-sm text-red-600">&nbsp;{keyError}</p>
         ) : null}
@@ -93,6 +148,7 @@ const HeaderRow = ({
           value={header.value}
           onChange={onChangeValue}
           placeholder="Value"
+          ref={valueRef}
         />
         {valueError ? (
           <p className="mt-2 text-sm text-red-600">&nbsp;{valueError}</p>
