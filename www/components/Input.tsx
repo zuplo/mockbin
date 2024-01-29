ab59fd4adec44b6bb4537e735b4d33e035e86243
Daniel <daniellehr@gmx.de>
2024-01-29 17:50:59 +0100

    feat: Add header name suggestions (#31)

--- www/components/Input.tsx
+++ www/components/Input.tsx
@@ -1,28 +1,30 @@
 import React from "react";
 import cn from "classnames";
 
-const Input = ({
-  value,
-  onChange,
-  className,
-  textarea,
-  ...props
-}: { textarea?: boolean } & React.InputHTMLAttributes<
-  HTMLInputElement | HTMLTextAreaElement
->) => {
+type InputProps = {
+  textarea?: boolean;
+} & React.InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement>;
+
+const Input = React.forwardRef<
+  HTMLInputElement | HTMLTextAreaElement,
+  InputProps
+>(({ value, onChange, className, textarea, ...props }, ref) => {
   const Comp = textarea ? "textarea" : "input";
   return (
     <Comp
+      ref={ref as any}
       value={value}
       onChange={onChange}
       className={cn(
         "bg-transparent border font-mono p-1 px-2 border-slate-700 flex-grow rounded",
-        "focus:ring-4 focus:outline-none focus:ring-slate-700/50",
+        "focus:ring-4 focus:outline-none focus:ring-slate-700/50 hover:bg-black/25",
         className,
       )}
       {...props}
     />
   );
-};
+});
+
+Input.displayName = "Input";
 
 export default Input;
