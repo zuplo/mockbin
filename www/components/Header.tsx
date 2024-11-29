e03961ac088b3ee482c2e8ba9d79c46f1a9098d1
Josh Twist <josh@molmorg.com>
2024-11-29 14:19:35 +0000

    added new logo

--- www/components/Header.tsx
+++ www/components/Header.tsx
@@ -1,23 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import Link from "next/link";
-import HeaderImage from "../public/mockbin-header.png";
-import RMOLink from "@/components/RMOLink";
+import HeaderImage from "../public/mockbin-white.svg";
 
 const Header = () => {
   return (
     <header className="flex flex-row w-full justify-between items-center">
       <Link href="/">
-        <Image width={360} height={132} alt="mockbin logo" src={HeaderImage} />
+        <Image
+          height={40}
+          alt="mockbin logo"
+          src={HeaderImage}
+          className="py-4"
+        />
       </Link>
       <div className="flex gap-x-2 items-center">
-        <a href="https://github.com/zuplo/mockbin" target="_blank">
-          <img
-            alt="GitHub Repo stars"
-            src="https://img.shields.io/github/stars/zuplo/mockbin?link=https%3A%2F%2Fgithub.com%2Fzuplo%2Fmockbin"
-          />
-        </a>
-        <RMOLink />
         <a
           target="_blank"
           className="hidden sm:flex items-center gap-x-1 bg-[#5865F2] h-[31px] border border-[#464ec7] hover:bg-[#464ec7] p-2 py-[4px] rounded-[4px]"
