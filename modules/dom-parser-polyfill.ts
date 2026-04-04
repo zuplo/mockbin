5c6ef5516fd94ca73fc0e035ca1669f1aea18d06
Nick Chen <nick@zuplo.com>
2026-04-03 19:11:23 -0700

    Expand Mockbin DOM polyfill globals

--- modules/dom-parser-polyfill.ts
+++ modules/dom-parser-polyfill.ts
@@ -5,3 +5,28 @@ import xmldom from "./third-party/xmldom/index.js";
 if (typeof globalThis.DOMParser === "undefined") {
   globalThis.DOMParser = xmldom.DOMParser as typeof DOMParser;
 }
+
+if (typeof globalThis.Node === "undefined") {
+  globalThis.Node = xmldom.Node as typeof Node;
+}
+
+if (typeof globalThis.Element === "undefined") {
+  globalThis.Element = xmldom.Element as typeof Element;
+}
+
+if (typeof globalThis.Document === "undefined") {
+  globalThis.Document = xmldom.Document as typeof Document;
+}
+
+if (typeof globalThis.DocumentFragment === "undefined") {
+  globalThis.DocumentFragment =
+    xmldom.DocumentFragment as typeof DocumentFragment;
+}
+
+if (typeof globalThis.NodeList === "undefined") {
+  globalThis.NodeList = xmldom.NodeList as typeof NodeList;
+}
+
+if (typeof globalThis.XMLSerializer === "undefined") {
+  globalThis.XMLSerializer = xmldom.XMLSerializer as typeof XMLSerializer;
+}
